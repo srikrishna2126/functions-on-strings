e7bc298e27dd63d8e6e180bc107f9f27e5c2c6df
sri krishna <51322416+srikrishna2126@users.noreply.github.com>
2019-06-25 21:57:16 +0530

    in bulit functions

---
+++ ALL FUNCTIONS ON STRINGS.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  1 17:13:42 2019
+
+@author: HAI
+"""
+
+p=12
+print(p)
+print(int(p))
+print(float(p))
+print(str(p))
+
+
+s='python'
+print(s[0])
+print(s[0],s[1],s[2],s[3],s[4],s[5])
+print(s[-1],s[-2],s[-3],s[-4],s[-5],s[-6])
+print(s[0:4])
+print(s[ :4])
+print(s[0: ])
+print(s[0: :1])
+print(s[ : :1])
+print(s[ : :2])
+print(s[ : :3])
+print(s[0:4:1])
+print(s[0:4:2])
+print(s[0: :2])
+print(s[0: :-1])
+print(s[0: :-1])
+print(s[0:6:-2])
+print(s[ : :-2])
+print(s[2: :-1])
+print(s[ : :-1])
+g='123'
+print(g[3: :-1])
+print(g[6:1:-1])
+print(g[ : :-1])
+print(g.startswith('s'))
+print(g.endswith('r'))
+print(g.isupper())
+print(g.islower())
+print(g.isalpha())
+print(g.isalnum())
+print(g.istitle())
+print(g.isdigit())
+print(g.swapcase())
+print(g.upper())
+print(g.lower())
+print(g.title())
+print(g.find('h'))
+print(g.find('a'))
+print(g.find('a',5))
+print(g.index('d'))
+print(g.count('a'))
+print(g.replace('a','w'))
+
+
